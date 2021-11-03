5ecc734050bccaadbd7bef84a8c9345fa1390d3b
LeaskN <leask.nicholas@gmail.com>
2021-11-02 20:25:50 -0400

    day1 notes

---
+++ 01-JavaScript-Basics/1-data-types-variables/0-data-types-overview/0-data-types-notes.js
@@ -0,0 +1,20 @@
+// console.log('Hello World');
+// console.log("Hello World");
+// console.log(`Hello World`);
+
+// console.log("This is a test '' <-quotes")
+// console.log("This is Thomas' first day")
+
+// console.log(5+3) // 8
+// console.log(5*3) // 15
+// console.log(6/3) // 2
+
+
+// Strings
+let text = "lorem ipsum";
+console.log(text);
+console.log(text[0]);
+console.log(text.length);
+
+// Numbers
+
