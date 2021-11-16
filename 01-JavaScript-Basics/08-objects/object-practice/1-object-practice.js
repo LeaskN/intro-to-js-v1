d1dba457d5e1ee12df58262a9af111faae877778
leaskn <leask.nicholas@gmail.com>
2021-11-16 18:19:44 -0500

    updated objects

---
+++ 01-JavaScript-Basics/08-objects/object-practice/1-object-practice.js
@@ -0,0 +1,10 @@
+// 1.   Create an athlete for a celebrity and save it to a variable
+
+// 2.   Using dot-notation add 3 key-value pairs to the celebrity
+
+// 3.   Using bracket-notation add 3 key-value pairs to the celebrity
+
+// 4.   Write a function that allows us to add or update 3 properties
+
+// 5.   Using a loop - log all the celebrities properties to the console
+
